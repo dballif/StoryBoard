e6bef0197b05d7a8b60a5bcfa6324de2aa9a6f14
Daniel Ballif <ballifdaniel@gmail.com>
2023-08-16 08:11:59 -0400

    Add some initial work to flask
    Added the basis for hosting hte form
    
    Renamed index.html to form.html to better represent its contents

--- storyboard.py
+++ storyboard.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return "<p>Hello, World!</p>"
+    return render_template("form.html")
+
+@app.route('/submit-starter-form', methods =["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       first_name = request.form.get("fname")
+       # getting input with name = lname in HTML form
+       last_name = request.form.get("lname")
+       return "Your name is "+first_name + last_name
+    return render_template("form.html")
+
+app.run()
