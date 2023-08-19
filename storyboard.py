50416c35f566a6d348425fe29cc0814dfa55a360
Daniel Ballif <ballifdaniel@gmail.com>
2023-08-18 20:24:54 -0400

    Start Form Backend
    Add python method to handle form input
    Added a recieved page
    Add some styling
    Start Stable Diffusion tool in tools.py

--- storyboard.py
+++ storyboard.py
@@ -1,4 +1,9 @@
 from flask import Flask, request, render_template
+from tools import generateFrames
+import sys
+import html
+
+sys.path.insert(0, 'tools/')
 
 app = Flask(__name__)
 
@@ -9,11 +14,17 @@ def main_page():
 @app.route('/submit-starter-form', methods =["GET", "POST"])
 def gfg():
     if request.method == "POST":
-       # getting input with name = fname in HTML form
-       first_name = request.form.get("fname")
-       # getting input with name = lname in HTML form
-       last_name = request.form.get("lname")
-       return "Your name is "+first_name + last_name
-    return render_template("form.html")
+        #Get Project Title
+        project_title = html.escape(request.form.get("title"))
+        
+        #Get Project Description
+        project_description = html.escape(request.form.get("description"))
+        #Get Type
+        storyboard_type = html.escape(request.form.get("storyboard-type"))
+        #Get Full Description - Contains all the frames separated by +
+        frame_descriptions = html.escape(request.form.get("storyboard-frames"))
+        #Use this function to actually generate pics and return array of files
+        picArray = generateFrames(frame_descriptions, storyboard_type)
+    return render_template("received.html", titleContent=project_title, descriptionContent=project_description, links=picArray)
 
 app.run()
---
+++ tools/tools.py
@@ -0,0 +1,38 @@
+import html
+from diffusers import StableDiffusionPipeline
+
+def splitIntoArray(frame_sentences):
+    return frame_sentences.split("+")
+
+def generateFrames(frame_sentences, style):
+	newArray = splitIntoArray(frame_sentences)
+	promptNum = 0;
+	nameArray = []
+    
+	#Loop through the frames and generate images
+	for frame in newArray:
+		promptNum = promptNum + 1
+		frameName = "prompt" + promptNum + ".png"
+
+		#Insert stable diffusion here
+		base = StableDiffusionPipeline.from_single_file('../data/mdjrny-v4.safetensors', use_safetensors=True) 
+		#Build the prompt
+		base_prompt = "masterpiece, best quality, photorealistic, 8k, detailed, dramatic,"
+		full_prompt = base_prompt + style + ", " + frame
+
+		#Use a negative prompt to mtry to make picture better
+		neg_prompt = "low res, ugly, bad hands, too many digits, bad teeth, blurry, blurred background"
+		
+		#V1 - Use cpu
+		base.to("cpu")
+
+		#Create the image
+		image = base(prompt=full_prompt, negative_prompt=neg_prompt).images[0]
+		
+		#Save each image into images folder with name prompt#.png
+		image.save()
+		#Add name to array
+		nameArray.append(frameName)
+    
+	#Return an array with the name of each file
+	return nameArray
